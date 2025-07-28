ea20e94ba0d61ea54328680e6771c7a7787f5cec
icirea <ioan.cirea@yahoo.com>
2025-07-28 20:33:35 +0200

    WIP: Neural network

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,37 @@
+
+#[derive(Debug)]
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: Vec<f32>) -> f32 {
+        todo!()
+    }
+}
